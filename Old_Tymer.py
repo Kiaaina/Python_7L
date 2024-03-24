6084823b0044456b9ba2eab6af31b5e7284f9231
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-23 19:09:25 -1000

    Update Old_Tymer.py

--- Old_Tymer.py
+++ Old_Tymer.py
@@ -1,7 +1,7 @@
 print("I Know Your 100d Birthday")
 val1=int(input("What Yeare is Today?: "))
 val2=int(input("What Year Were You Born?: "))
-val3=int(input("Month Your born?: "))
+val3=int(input("Month Your Born?: "))
 print("You will be 100d years old in year")
 print(100+val2)
 print("Your Age On Month",val3,"is")
