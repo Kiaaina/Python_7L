483560639157bd23a7774a349eb67144a7b45382
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-23 19:58:39 -1000

    Add files via upload

---
+++ Python.apk.py
@@ -0,0 +1,7 @@
+print("python.apk")
+"https://www.apkmirror.com/apk/iiec/pydroid-3-ide-for-python-3/pydroid-3-ide-for-python-3-7-2-release/pydroid-3-ide-for-python-3-7-2_x86_64-android-apk-download/"
+
+
+
+    
+    
---
+++ python.apk.cmd
@@ -0,0 +1,17 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "python_apk"
+@cecho off 
+@set "py=https://www.apkmirror.com/apk/iiec/pydroid-3-ide-for-python-3/pydroid-3-ide-for-python-3-7-2-release/pydroid-3-ide-for-python-3-7-2_x86_64-android-apk-download/"
+cecho {04}Python.apk{07}
+
+:one
+start %py%
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+@timeout /t 5
+exit
