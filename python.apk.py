05f9b3963cd135695edc25bafe7b512448e58711
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-23 21:59:23 -1000

    Delete python.apk.py

--- python.apk.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-print("python.apk")
-webbrowser.open('https://www.apkmirror.com/apk/iiec/pydroid-3-ide-for-python-3/pydroid-3-ide-for-python-3-7-2-release/pydroid-3-ide-for-python-3-7-2_arm-android-apk-download/')
-
-
