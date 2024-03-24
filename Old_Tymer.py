e39e4883e14c766f5c4e719891699a3059f68e33
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-23 19:08:38 -1000

    Add files via upload

--- Old_Tymer.py
+++ Old_Tymer.py
@@ -4,9 +4,8 @@ val2=int(input("What Year Were You Born?: "))
 val3=int(input("Month Your born?: "))
 print("You will be 100d years old in year")
 print(100+val2)
-print("Your Age is")
+print("Your Age On Month",val3,"is")
 print(val1-val2,"Years old")
-print("On month",val3)
 
 
 
