f95cf6b1430d04adddef88e08c0a9e1d8692d19a
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-24 15:50:24 -1000

    Add files via upload

---
+++ Dogy_Years.py
@@ -0,0 +1,10 @@
+print("Whats Your Dogs Age?")
+val0=int(input("The Year Today?: "))
+val1=int(input("The Year your Dog was Born?: "))
+val2=int(input("Month Dog is Born?: "))
+val3=(val0-val1)
+print("On Month",val2)
+print("Your Dog's Age is",val3+7,"Years old")
+
+
+    
