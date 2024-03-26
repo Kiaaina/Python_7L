15eb552094847089f70ec5478c8d21fec2a5f333
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-25 14:00:51 -1000

    Add files via upload

---
+++ Legal_Cigarettes_Age.py
@@ -0,0 +1,12 @@
+print("\033[1;31;40mLegal Age 4 Cigarettes")
+val1=int(input("What Year is Today?: "))
+val2=int(input("What Year Were You Born?: "))
+val3=int(input("Month Your Born?: "))
+val4=int(input("Day Your Born: "))
+print("You can Smoke at age 21")
+print("On",val3,"/",val4,"/",val1)
+print("You Are",val1-val2,"Years old")
+
+
+
+    
