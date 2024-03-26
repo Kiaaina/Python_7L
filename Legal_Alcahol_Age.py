ef857c47dc1b424ad0807d3440aac20a56104e15
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-25 14:00:21 -1000

    Add files via upload

---
+++ Legal_Alcahol_Age.py
@@ -0,0 +1,12 @@
+print("\033[1;31;40mLegal Age 4 Alcohal")
+val1=int(input("What Year is Today?: "))
+val2=int(input("What Year Were You Born?: "))
+val3=int(input("Month Your Born?: "))
+val4=int(input("Day Your Born: "))
+print("You can Drink at age 21")
+print("On",val3,"/",val4,"/",val1)
+print("You Are",val1-val2,"Years old")
+
+
+
+    
